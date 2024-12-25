bea432bf325d24cfb0d5cc1b26c3729956330c2c
Joep Schuurkes <j19sch@gmail.com>
2024-12-25 10:33:27 +0100

    expands counterstring up to length 100

--- src/counterstring.test.ts
+++ src/counterstring.test.ts
@@ -5,7 +5,12 @@ import { counterstring } from './counterstring'
 test.each([
     [0, ""],
     [1, "*"],
-    [2, "2*"]
+    [2, "2*"],
+    [3, "*3*"],
+    [4, "2*4*"],
+    [9, "*3*5*7*9*"],
+    [10, "*3*5*7*10*"],
+    [100, "*3*5*7*9*12*15*18*21*24*27*30*33*36*39*42*45*48*51*54*57*60*63*66*69*72*75*78*81*84*87*90*93*96*100*"]
 ])("counterstring length %i", (length, expected) => {
     expect(counterstring(length)).toBe(expected);
 })
--- src/counterstring.ts
+++ src/counterstring.ts
@@ -1,18 +1,24 @@
 export function counterstring(length:number) {
+    const token:string = "*"
+    const tokenLength:number = token.length
+
     let stringList: string[] = [];
     stringList.push("")
 
     let latestTokenPosition;
+    let insertLength;
 
     while (length > 0) {
         if (latestTokenPosition) {
             stringList.push(latestTokenPosition.toString())
+            insertLength = latestTokenPosition.toString().length
             latestTokenPosition = null
         } else {
-            stringList.push("*")
+            stringList.push(token)
             latestTokenPosition = length
+            insertLength = tokenLength
         }
-        length -= 1
+        length -= insertLength
     }
 
     const counterString = stringList.reverse().join("")
