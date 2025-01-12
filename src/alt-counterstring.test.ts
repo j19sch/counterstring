06766868f8afb70b2a49ea6670c4f65717a55a45
Joep Schuurkes <j19sch@gmail.com>
2025-01-12 09:20:34 +0100

    fixes printing function name in tests

--- src/alt-counterstring.test.ts
+++ src/alt-counterstring.test.ts
@@ -9,16 +9,14 @@ import {
   templateString,
 } from "./alt-counterstrings";
 
-// TODO: function %s not prints full function
-
 describe.each([
-  [reverseList],
-  [templateString],
-  [concatenateString],
-  [makeCS],
-  [evilTester],
-  [onlyOneReverse],
-])(`function %s`, (counterstringFunction) => {
+  { fn: reverseList, fnName: reverseList.name },
+  { fn: templateString, fnName: templateString.name },
+  { fn: concatenateString, fnName: concatenateString.name },
+  { fn: makeCS, fnName: makeCS.name },
+  { fn: evilTester, fnName: evilTester.name },
+  { fn: onlyOneReverse, fnName: onlyOneReverse.name },
+])(`function $fnName`, ({ fn }) => {
   test.each([
     [0, ""],
     [1, "*"],
@@ -32,6 +30,6 @@ describe.each([
       "*3*5*7*9*12*15*18*21*24*27*30*33*36*39*42*45*48*51*54*57*60*63*66*69*72*75*78*81*84*87*90*93*96*100*",
     ],
   ])("counterstring length %i", (length, expected) => {
-    expect(counterstringFunction(length)).toBe(expected);
+    expect(fn(length)).toBe(expected);
   });
 });
